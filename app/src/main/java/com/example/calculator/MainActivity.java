71bfb3d81f0cde446431d70d1081ed6ee325003d
татьяна <emuart155@gmail.com>
2021-05-22 20:36:39 +0300

    Добавила кнопки в массив, чтобы проще было работать

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final char ADDITION = '+';
@@ -15,6 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] numberIds = new int[]{
+                R.id.buttonZero,
+                R.id.buttonOne,
+                R.id.buttonTwo,
+                R.id.buttonThree,
+                R.id.buttonFour,
+                R.id.buttonFive,
+                R.id.buttonSix,
+                R.id.buttonSeven,
+                R.id.buttonEight,
+                R.id.buttonNine
+
+        };
+
+        int[] actionsIds = new int[]{
+                R.id.buttonAC,
+                R.id.buttonC,
+                R.id.buttonAddition,
+                R.id.buttonDot,
+                R.id.buttonMultiplication,
+                R.id.buttonDivision,
+                R.id.buttonPercent,
+                R.id.buttonSubtraction,
+                R.id.buttonEqual
+        };
 
     }
 }
