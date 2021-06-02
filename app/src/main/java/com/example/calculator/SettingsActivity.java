d14d44ef580c8df41007f4bd2a9b46341f43f0d8
татьяна <emuart155@gmail.com>
2021-06-02 20:33:44 +0300

    Некоторые доработки

--- app/src/main/java/com/example/calculator/SettingsActivity.java
+++ app/src/main/java/com/example/calculator/SettingsActivity.java
@@ -12,7 +12,6 @@ import com.google.android.material.radiobutton.MaterialRadioButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private static final String NAME_SHARED_PREFERENCE = "LOGIN";
     private static final String APP_THEME = "Theme_Calculator";
 
     private static final int APP_THEME_LIGHT_CODE_STYLE = 0;
@@ -35,14 +34,15 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
     }
-
+//инициализация кнопок
     private void initThemeChooser() {
         initRadioButton(findViewById(R.id.radioButtonMaterialLight),
                 APP_THEME_LIGHT_CODE_STYLE);
         initRadioButton(findViewById(R.id.radioButtonMaterialDark),
                 APP_THEME_DARK_CODE_STYLE);
         RadioGroup rg = findViewById(R.id.radioButtons);
-        ((MaterialRadioButton) rg.getChildAt(getCodeStyle(APP_THEME_LIGHT_CODE_STYLE))).setChecked(true);
+        ((MaterialRadioButton) rg.getChildAt(getCodeStyle(APP_THEME_LIGHT_CODE_STYLE)))
+                .setChecked(true);
     }
 
     // Все инициализации кнопок очень похожи, поэтому создадим метод для переиспользования
@@ -62,21 +62,23 @@ public class SettingsActivity extends AppCompatActivity {
         return codeStyleToStyleId(getCodeStyle(codeStyle));
     }
 
+    // Сохранение настроек
+    private void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
+        // Настройки сохраняются посредством специального класса editor.
+        SharedPreferences.Editor editor = sharedPref.edit();
+        //Сохраняем настройки: ключ-значение
+        editor.putInt(APP_THEME, codeStyle);
+        //отложенное сохранение
+        editor.apply();
+    }
+
     // Чтение настроек, параметр «тема»
     private int getCodeStyle(int codeStyle) {
         // Работаем через специальный класс сохранения и чтения настроек
-        SharedPreferences sharedPref = getSharedPreferences(NAME_SHARED_PREFERENCE, MODE_PRIVATE);
+        SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
         //Прочитать тему, если настройка не найдена - взять по умолчанию
         return sharedPref.getInt(APP_THEME, codeStyle);
-    }
-
-    // Сохранение настроек
-    private void setAppTheme(int codeStyle) {
-        SharedPreferences sharedPref = getSharedPreferences(NAME_SHARED_PREFERENCE, MODE_PRIVATE);
-        // Настройки сохраняются посредством специального класса editor.
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(APP_THEME, codeStyle);
-        editor.apply();
     }
 
     private int codeStyleToStyleId(int codeStyle) {
